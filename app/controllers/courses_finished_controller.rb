dbaf16f18be482dd11cc2803ae58ab645567d92a
Mauro David Almiron <mdavid.almiron@gmail.com>
2022-01-07 18:05:09 -0300

    Separacion de cursos finalizados
    Se separa con un controlador por si mas adelante piden logica sobre cursos terminados

---
+++ app/controllers/courses_finished_controller.rb
@@ -0,0 +1,11 @@
+class CoursesFinishedController < ApplicationController
+  def index
+    limit_date = Time.now.strftime("%Y-%m-%d")
+    @courses = Course.where(active: :true).where("finish_date < '#{ limit_date }'")
+    @breadcrumbs = [
+      [ :name =>'Cursos', :path => courses_path],
+      [ :name =>'Cursos finalizados', :path => ''] 
+    ]
+    @nav_active = [:clients=> '', :courses=> 'active', :programs=> '']
+  end
+end
