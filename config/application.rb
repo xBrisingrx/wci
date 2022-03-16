3161693630b45d07aeacf67f43048f5955ed5bd0
Mauro Almiron <mdavid.almiron@gmail.com>
2022-03-16 19:34:48 -0300

    Traduccion espanol
    Archivo de traduccion
    Seteo de espanol por defecto

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Wci
     #config zona horaria
     config.time_zone = 'Buenos Aires'
     config.active_record.default_timezone = :utc
+    config.i18n.default_locale = :es
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
