6fbb7c91951ab8513f1cd165020182444ddb153b
Mauro Almiron <mdavid.almiron@gmail.com>
2025-05-05 01:09:56 -0300

    test

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,10 +2,8 @@ class ContactMailer < ApplicationMailer
   default from: "soporte@maurosampaoli.com.ar"
 
   def contact_email # este email nos lo envian desde la web de webcontroll.la
-    # @email = params[:email]
     @name = params[:name]
     @email = params[:email]
-    @params = params[:email]
-    mail(to: params[:email], subject: "Mensaje desde formulario Sitio wellcontrol.la")
+    mail(to: @email, subject: "Mensaje desde formulario Sitio wellcontrol.la")
   end
 end
