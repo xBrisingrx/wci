f8fc880c3d7c0cdfa154bf4705a1a2e6229b3d1d
Mauro Almiron <mdavid.almiron@gmail.com>
2025-05-05 15:22:48 -0300

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,8 @@ Rails.application.configure do
     port:            587,
     authentication: :plain,
     user_name:      'apikey',
-    password:       ENV['SENDGRID_API_KEY']
+    password:       ENV['SENDGRID_API_KEY'],
+    enable_starttls_auto: true
   }
 
 
