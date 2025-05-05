9ccc77fc1805ff3fb056e0f0b983c87198abf997
Mauro Almiron <mdavid.almiron@gmail.com>
2025-05-05 00:58:14 -0300

    test

--- app/controllers/web_email_controller.rb
+++ app/controllers/web_email_controller.rb
@@ -3,8 +3,7 @@ class WebEmailController < ApplicationController
   skip_before_action :no_login
 
   def contact_email
-    email = params[:email]
-    ContactMailer.with(name: email_params[:name], email: email).contact_email.deliver_later
+    ContactMailer.with(name: email_params[:name], params[:email]).contact_email.deliver_later
     render json: {data: email_params, status: 'success'}
   end
 
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,11 @@
 class ContactMailer < ApplicationMailer
   default from: "soporte@maurosampaoli.com.ar"
 
-  def contact_email(name, email) # este email nos lo envian desde la web de webcontroll.la
+  def contact_email # este email nos lo envian desde la web de webcontroll.la
     # @email = params[:email]
-    @name = name
-    @params = params
-    mail(to: email, subject: "Mensaje desde formulario Sitio wellcontrol.la")
+    @name = params[:name]
+    @email = params[:email]
+    @params = params[:email]
+    mail(to: params[:email], subject: "Mensaje desde formulario Sitio wellcontrol.la")
   end
 end
