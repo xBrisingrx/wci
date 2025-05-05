9afc91e2e60a28b351a62a55756e06665cc9a10b
Mauro Almiron <mdavid.almiron@gmail.com>
2025-05-05 00:33:12 -0300

    fix typo

--- app/controllers/web_email_controller.rb
+++ app/controllers/web_email_controller.rb
@@ -3,7 +3,7 @@ class WebEmailController < ApplicationController
   skip_before_action :no_login
 
   def contact_email
-    email: params[:email]
+    email = params[:email]
     ContactMailer.with(params: email_params, email: email).contact_email.deliver_later
     render json: {data: email_params, status: 'success'}
   end
