33cb619bfc2d106964e7f730c35e872dc5f79001
Mauro Almiron <mdavid.almiron@gmail.com>
2025-05-04 23:50:36 -0300

    config email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         "imutes.servidoraweb.net",
+    port:            587,
+    domain:          "wci.la",
+    user_name:       ENV['EMAIL_USERNAME'],
+    password:        ENV['EMAIL_PASSWORD'],
+    authentication:  "plain"
+  }
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
